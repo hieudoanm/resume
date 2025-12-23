a867495edf3f3b6930fead1ed39458b11dc3f321
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-12-24 00:13:49 +0700

    initial

---
+++ docs/_next/static/Bu6LXB4DWUtuZGgfSTJEj/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/daace953e58534f5.js"
+  ],
+  "/_error": [
+    "static/chunks/beef212b8d60c268.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
