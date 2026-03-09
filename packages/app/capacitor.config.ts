ba777f2ee436aec3d3609055f901d65e217ba502
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 22:50:34 +0700

    update cap

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
 	appId: 'io.github.hieudoanm.resume',
-	appName: '@redact/frontend',
+	appName: 'Resume',
 	webDir: '../../docs',
 	android: { path: 'mobile/android' },
 	ios: { path: 'mobile/ios' },
